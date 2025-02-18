3ed216da9e377a78a6fe45bc566494987351cb47
trubicynaite <trubicynaite@gmail.com>
2025-02-18 20:02:15 +0200

    added x functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document
-    .querySelectorAll(".tabLink").forEach(tabLink => {
+    .querySelectorAll(".tabLink")
+    .forEach(tabLink => {
         tabLink.addEventListener('click', (event) => {
             event.preventDefault();
 
@@ -24,6 +25,29 @@ const openTab = (event, tabName) => {
     tabLink.forEach(link => link.classList.remove("active"));
 
     document.getElementById(tabName).style.display = "block";
+    event.currentTarget.classList.add("active");
+};
+
+document
+    .querySelectorAll("#xmark")
+    .forEach(xmark => {
+        xmark.addEventListener("click", (event) => {
+            event.preventDefault();
+            closeTab(event);
+        });
+    });
+
+const closeTab = (event) => {
+    const notTabContent = document.querySelectorAll("#notTabContent");
+    notTabContent.forEach(content => content.style.display = "grid");
+
+    const tabs = document.querySelectorAll(".tabs");
+    tabs.forEach(content => content.style.display = "grid");
+
+    const tabContent = document.querySelectorAll(".tabsContent");
+    tabContent.forEach(content => content.style.display = "none");
+
+    event.currentTarget.style.display = "none";
     event.currentTarget.classList.add("active");
 };
 
